6f78075c90517c6dda2a85e7b35f578006481464
zjamali <zouhairjamali1998@gmail.com>
2022-03-04 19:41:01 +0100

    add document of request

--- micro_server.cpp
+++ micro_server.cpp
@@ -6,8 +6,7 @@
 
 int main()
 {
-    while(1)
-    {
+
     // Create a socket (IPv4, TCP)
     int sockfd = socket(AF_INET, SOCK_STREAM, 0);
     if (sockfd == -1)
@@ -20,42 +19,85 @@ int main()
     sockaddr_in sockaddr;
     sockaddr.sin_family = AF_INET;
     sockaddr.sin_addr.s_addr = INADDR_ANY;
-    sockaddr.sin_port = htons(9999); // htons is necessary to convert a number to
+    sockaddr.sin_port = htons(9998); // htons is necessary to convert a number to
                                      // network byte order
     if (bind(sockfd, (struct sockaddr *)&sockaddr, sizeof(sockaddr)) < 0)
-    { 
+    {
         std::cout << "Failed to bind to port 9999. errno: " << errno << std::endl;
         exit(EXIT_FAILURE);
     }
-    
-    
-        // Start listening. Hold at most 10 connections in the queue
-        if (listen(sockfd, 10) < 0)
-        {
-            std::cout << "Failed to listen on socket. errno: " << errno << std::endl;
-            exit(EXIT_FAILURE);
-        }
 
-        // Grab a connection from the queue
-        size_t addrlen = sizeof(sockaddr);
+    // Start listening. Hold at most 10 connections in the queue
+    if (listen(sockfd, 10) < 0)
+    {
+        std::cout << "Failed to listen on socket. errno: " << errno << std::endl;
+        exit(EXIT_FAILURE);
+    }
+
+    // Grab a connection from the queue
+    size_t addrlen = sizeof(sockaddr);
+    while (1)
+    {
+        /* code */
+
         int connection = accept(sockfd, (struct sockaddr *)&sockaddr, (socklen_t *)&addrlen);
         if (connection < 0)
         {
             std::cout << "Failed to grab connection. errno: " << errno << std::endl;
             exit(EXIT_FAILURE);
         }
-
-        // Read from the connection
         char buffer[100];
-        int bytesRead = read(connection, buffer, 100);
+        int bytesRead = read(connection, buffer, 1000);
         (void)bytesRead;
-        std::cout << "The message was: " << buffer;
-
-        // Send a message to the connection
+        std::cout << buffer;
         std::string response = "Good talking to you\n";
         send(connection, response.c_str(), response.size(), 0);
         close(connection);
-        close(sockfd);
     }
+    close(sockfd);
+    // Read from the connection
+
+    // Send a message to the connection
     // Close the connections
 }
+
+/*
+parsing request, set status code
+parse first header -> method: [GET, POST, DELETE], url(3la 7ssab config file), protocol = HTTP/1.1
+everythings else: map[key] = value
+
+example for post method:
+special variable: std::string postbody
+POST / HTTP/1.1
+Host: localhost:9998
+Content-Type: application/json
+Content-Length: 15
+
+{
+    "t": 5,
+    "a": 6
+}
+
+example for post method with files:
+struct fileParse {
+    std:string ConDisposition;
+    std:string ContType;
+    std::string body;
+}
+special variable for files: std::vector<struct fileParse> v;
+POST / HTTP/1.1
+Host: localhost:9998
+other-headers
+Content-Type: multipart/form-data; boundary=----WebKitFormBoundaryePkpFF7tjBAqx29L
+
+------WebKitFormBoundaryePkpFF7tjBAqx29L
+Content-Disposition: form-data; name="MAX_FILE_SIZE"
+
+100000
+------WebKitFormBoundaryePkpFF7tjBAqx29L
+Content-Disposition: form-data; name="uploadedfile"; filename="hello.o"
+Content-Type: application/x-object
+
+... contents of file goes here ...
+------WebKitFormBoundaryePkpFF7tjBAqx29L--
+*/
