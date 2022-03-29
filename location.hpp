9e47b0b65e9668442602cf08b2c53dfc3ae7d2aa
iltafah <eliastafah@gmail.com>
2022-03-29 13:43:53 +0000

    Time for server and location to shine

---
+++ location.hpp
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   location.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: iltafah <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/29 11:44:42 by iltafah           #+#    #+#             */
+/*   Updated: 2022/03/29 13:38:58 by iltafah          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LOCATION_HPP
+# define LOCATION_HPP
+
+class location
+{
+	private:
+		//location path
+		//autoindex {bool}
+		//indecies {maybe a vector of strings}
+		//allow methods {better to use map}
+		//return code and path
+		
+
+	public:
+		location();
+		~location();
+};
+
+#endif
